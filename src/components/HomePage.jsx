21ad379f8ede8bda2278913d4a14aba1f01997e5
Pablo Cigoy <pablocigoy@gmail.com>
2023-03-14 18:58:25 +0100

    bg home

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -6,11 +6,11 @@ export const HomePage = () => {
             <div
                 className="h-screen bg-center bg-cover "
                 style={{
-                    backgroundImage: "url('https://res.cloudinary.com/be-chef/image/upload/v1678813487/mf2ijzabwncyjr28swmj.jpg')",
+                    backgroundImage: "url('https://res.cloudinary.com/be-chef/image/upload/v1678816068/pnvfo46fuze8vxng5aea.jpg')",
                 }}
             >
                 <div className="h-full flex items-center justify-center">
-                    <h1 className="text-white font-bold text-5xl">
+                    <h1 className="text-white font-bold text-5xl ml-32">
                         mi blog
                     </h1>
                 </div>
